ac6fa9a0d7349ad892851404e478c4d60e9fb6ad
msebaf <msebaf@hotmail.com>
2023-03-25 20:37:42 -0300

    base

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Inmobiliaria.Net.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
